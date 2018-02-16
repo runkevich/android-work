26e04a173c96718a797e024063494f61e1b3829d
runkevich <runkevich8@gmail.com>
2018-02-17 01:39:52 +0300

    add

---
+++ Test2/app/src/main/java/com/runkevich8/gmail/test/homework/HomeworkMain.java
@@ -0,0 +1,54 @@
+package com.runkevich8.gmail.test.homework;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.runkevich8.gmail.test.R;
+import com.runkevich8.gmail.test.homework.hw1.Homework1Activity;
+import com.runkevich8.gmail.test.homework.hw2.Homework2Activity;
+
+public class HomeworkMain extends AppCompatActivity {
+
+    private Button hw1;
+    private Button hw2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homework_main);
+
+        hw1 = findViewById(R.id.hw_1);
+        hw2 = findViewById(R.id.hw_2);
+
+
+        View.OnClickListener hw1Click = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeworkMain.this,
+                        Homework1Activity.class);
+                startActivity(intent);
+            }
+        };
+
+        hw1.setOnClickListener(hw1Click);
+
+       // hw1.setText(BuildConfig.API_URL);
+
+        View.OnClickListener hw2Click = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeworkMain.this,
+                        Homework2Activity.class);
+                startActivity(intent);
+            }
+        };
+
+        hw2.setOnClickListener(hw2Click);
+    }
+}
+
---
+++ Test2/app/src/main/java/com/runkevich8/gmail/test/homework/hw3/Homework3Main.java
@@ -0,0 +1,18 @@
+package com.runkevich8.gmail.test.homework.hw3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Eka on 16.02.2018.
+ */
+
+public class Homework3Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
